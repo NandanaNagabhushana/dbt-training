8a6fd7058d1488b7704ef1bf253e30f80c680e53
Nandana Nagabhushana <nandana.sgn@gmail.com>
2024-06-12 09:20:50 +0000

    added staging and reporting models

--- models/example/STAGE/stg_orders.sql
+++ models/example/STAGE/stg_orders.sql
@@ -1,8 +1,8 @@
 select 
 o.orderid, o.orderdate,o.shipdate,o.shipmode, o.ordercostprice, o.ordersellingprice,
 o.ordersellingprice-o.ordercostprice as orderprofit,
-c.customername, c.segment, c.country,
-p.productname,p.category,p.subcategory
+c.customerid,c.customername, c.segment, c.country,
+p.productid, p.productname,p.category,p.subcategory
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
