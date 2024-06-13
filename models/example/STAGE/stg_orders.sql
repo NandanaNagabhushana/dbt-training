245d6ad0c6ee8eb73b0ad7046a1843da913fb1d3
Nandana Nagabhushana <nandana.sgn@gmail.com>
2024-06-13 06:35:53 +0000

    added macros

--- models/example/STAGE/stg_orders.sql
+++ models/example/STAGE/stg_orders.sql
@@ -2,9 +2,12 @@ select
 o.orderid, o.orderdate,o.shipdate,o.shipmode, o.ordercostprice, o.ordersellingprice,
 o.ordersellingprice-o.ordercostprice as orderprofit,
 c.customerid,c.customername, c.segment, c.country,
-p.productid, p.productname,p.category,p.subcategory
+p.productid, p.productname,p.category,p.subcategory,
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_products') }} as p
 on o.productid = p.productid
+
+{{limit_data_in_dev('orderdate')}}
