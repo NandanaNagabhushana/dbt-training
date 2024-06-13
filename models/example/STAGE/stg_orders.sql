85ecc0b5d46417355eca698e59ad3096b320f2f9
Nandana Nagabhushana <nandana.sgn@gmail.com>
2024-06-13 06:56:21 +0000

    added packages, seeds

--- models/example/STAGE/stg_orders.sql
+++ models/example/STAGE/stg_orders.sql
@@ -1,11 +1,24 @@
-select 
-o.orderid, o.orderdate,o.shipdate,o.shipmode, o.ordercostprice, o.ordersellingprice,
-o.ordersellingprice-o.ordercostprice as orderprofit,
-c.customerid,c.customername, c.segment, c.country,
-p.productid, p.productname,p.category,p.subcategory,
-{{ markup('ordersellingprice', 'ordercostprice') }} as markup
-from {{ ref('raw_orders') }} as o
-left join {{ ref('raw_customer') }} as c
-on o.customerid = c.customerid
-left join {{ ref('raw_products') }} as p
-on o.productid = p.productid
+select
+    {{ dbt_utils.generate_surrogate_key(["o.orderid", "c.customerid", "p.productid"]) }}
+    as sk,
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordercostprice,
+    o.ordersellingprice,
+    o.ordersellingprice - o.ordercostprice as orderprofit,
+    c.customerid,
+    c.customername,
+    c.segment,
+    c.country,
+    p.productid,
+    p.productname,
+    p.category,
+    p.subcategory,
+    {{ markup("ordersellingprice", "ordercostprice") }} as markup,
+    d.delivery_team
+from {{ ref("raw_orders") }} as o
+left join {{ ref("raw_customer") }} as c on o.customerid = c.customerid
+left join {{ ref("raw_products") }} as p on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d on o.shipmode = d.shipmode
